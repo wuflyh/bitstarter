627ac0e7198aa32e5bd89dd0287074ef0d59a7c2
feihong wu <wuflyh@gmail.com>
2013-07-22 23:56:21 -0700

    adding up images

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/'));
+
 
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('./index.html', 'utf8');
@@ -10,7 +12,8 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
